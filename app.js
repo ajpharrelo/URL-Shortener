73a9cc879b8c8d12a5d4525d1fe24d9ac3e6654b
Ajae Obaze <72343143+ajpharrelo@users.noreply.github.com>
2023-05-13 18:53:09 +0100

    fixed misspell

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars');
 const linkController = require('./controller/link');
 const app = express();
 
-const hostname = "https://localhost:8080";
+const hostname = "http://localhost:8080";
 
 // Handlebars engine setup
 app.engine('hbs', handlebars.engine({
